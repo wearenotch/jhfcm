a1e6515a4f46180a3da0ccd178cf16a40806fdf1
Domagoj Madunić <domagoj.madunic@ag04.com>
2021-04-21 11:43:57 +0200

    Initial version of jhfcm generated by JHipster-7.0.1

---
+++ src/main/java/com/ag04/jhfcm/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ag04.jhfcm.repository;
+
+import com.ag04.jhfcm.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/ag04/jhfcm/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.ag04.jhfcm.web.rest;
